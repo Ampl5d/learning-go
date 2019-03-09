7af60b251d1df38bbe5a742cb795d12aacdba921
Rich <rich.wzorek@gmail.com>
2019-03-09 07:23:42 -0500

    Added hello world

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello everyone, this is an awesome class")
+}
+
+// control flow
+// (1) sequence
+// (2) loop; iterative
+// (3) conditional
